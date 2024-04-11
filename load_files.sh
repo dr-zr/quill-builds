52a2cc5e13d7a47fa46a417d90d59de08ff32096
zirlman <dragec.96@live.com>
2024-04-11 11:35:16 +0200

    add files

---
+++ load_files.sh
@@ -0,0 +1,3 @@
+curl -o quill.snow.css https://cdn.jsdelivr.net/npm/quill@2.0.0-rc.5/dist/quill.snow.css
+curl -o quill.bubble.css https://cdn.jsdelivr.net/npm/quill@2.0.0-rc.5/dist/quill.bubble.css
+
